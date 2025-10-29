6f96edcf284746b8163fc99d111af1dcd1900eeb
vinicius500197 <vinicius.alimeida@escola.pr.gov.br>
2025-10-29 08:06:28 -0300

    .

---
+++ app.js
@@ -0,0 +1,14 @@
+function criaCartao(categoria, pergunta,resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = "
+        < div class="cartao_conteudo" >
+            <h3>${categoria}</h3>
+            <div class="cartao_conteudo_pergunta">
+                <P>${pergunta}</P>
+    <div class cartao_conteudo_resposta">
+    < p > ${resposta}</p>
+    </div>
+    </div>
+}
