853ae88af62132d23369e4453608d8551c693e97
vinicius500197 <vinicius.alimeida@escola.pr.gov.br>
2025-11-12 08:06:31 -0300

    .

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ function criaCartao(categoria, pergunta,resposta) {
     </div>
     </div>
 }
+let respostaEstaVisivel = false
+
+   function viraCartao(){
+     respostaEstaVisivel = !respostaEstaVisivel
+     cartao.classList.toggle('active',respostaEstaVisivel
+   }
+   cartao.addEventListener('click',viraCartao)
+   container.appendChild(cartao)
+}
