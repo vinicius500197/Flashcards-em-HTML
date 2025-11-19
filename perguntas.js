4ede114c7abf1662fbba3235f33ea04e73193792
vinicius500197 <vinicius.alimeida@escola.pr.gov.br>
2025-11-19 11:21:37 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python',
+    'O que é Python?',
     'O Python é uma linguagem de programação'
 )
+
 criaCartao(
     'Geografia',
-    'Qual a capital da França',
+    'Qual a capital da França?',
     'A capital da França é Paris'
 )
+
 criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
 criaCartao(
     'Lingua inglesa',
-    'como se diz oi em Inglês?',
-    'Oi em inglês é HI (RAI)'
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
